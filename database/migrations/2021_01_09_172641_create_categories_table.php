a4ce0cce349f466ac39553d14e20d444da47b752
Henrique Flávio de Souza <henriqueflaviodev@gmail.com>
2021-01-09 14:57:39 -0300

    Category table and model

---
+++ database/migrations/2021_01_09_172641_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20);
+            $table->string('slug', 20);
+            $table->foreignId('user_id');
+            $table->softDeletes();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unique(['name', 'user_id']);
+            $table->unique(['slug', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
