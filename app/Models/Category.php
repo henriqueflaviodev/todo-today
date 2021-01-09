0d7fd32098d9c1a77cbc8ab895f91d09236af4ce
Henrique Flávio de Souza <henriqueflaviodev@gmail.com>
2021-01-09 15:56:52 -0300

    Default categories seeding

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::saving(function ($user) {
+            $user->slug = Str::slug($user->name);
+
+            return $user;
+        });
+    }
 }
