bd66e4ee3d9c3c1c7b076906bc00336a43417a33
Henrique Flávio de Souza <henriqueflaviodev@gmail.com>
2021-01-09 15:01:57 -0300

    User seeder to create sysadmin (default user)

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => env('ADMIN_USER_NAME'),
+            'email' => env('ADMIN_USER_EMAIL'),
+            'password' => Hash::make(env('ADMIN_USER_PASS')),
+        ]);
+    }
+}
