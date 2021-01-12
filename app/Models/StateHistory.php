8bb4ee6d19bff03e8e04eee44d33ab405d9f0fe8
Henrique Flávio de Souza <henriqueflaviodev@gmail.com>
2021-01-11 23:56:54 -0300

    Task priority and state histories models and crate tables migrations

---
+++ app/Models/StateHistory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StateHistory extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2021_01_12_023329_create_state_histories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStateHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('state_histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('task_id');
+            $table->foreignId('state_id');
+            $table->date('ended_at')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->foreign('state_id')->references('id')->on('states');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('state_histories');
+    }
+}
