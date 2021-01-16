c7f7d6948df3876e112c214c68ddeea6f46159cb
Henrique Flávio de Souza <henriqueflaviodev@gmail.com>
2021-01-16 00:33:12 -0300

    Create tasks consult by state and category slugs

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class Task extends Model
 {
@@ -20,12 +22,12 @@ class Task extends Model
 
     public function currentState()
     {
-        return $this->hasOne(State::class);
+        return $this->belongsTo(State::class);
     }
 
     public function currentPriority()
     {
-        return $this->hasOne(Priority::class);
+        return $this->belongsTo(Priority::class);
     }
 
     public function stateHistories()
@@ -41,5 +43,51 @@ class Task extends Model
     public function notes()
     {
         return $this->hasMany(Note::class);
+    }
+
+    /**
+     * get tasks by state (mandatory) and/or category, and return all needed data to build a tasks list
+     * @param string $stateSlug
+     * @param string $categorySlug
+     */
+    public function findByStateAndCategorySlug(string $stateSlug, string $categorySlug = '')
+    {
+        /*
+        return DB::table('tasks')
+            ->join('priorities', 'tasks.current_priority_id', '=', 'priorities.id')
+            ->join('states', function($join) use ($stateSlug) {
+                $join->on('tasks.current_state_id', '=', 'states.id')
+                    ->where('states.slug', '=', $stateSlug);
+            })
+            ->join('categories', function($join) use ($categorySlug) {
+                $join->on('tasks.category_id', '=', 'categories.id')
+                    ->where('categories.user_id', '=', Auth::id())
+                    ->when($categorySlug, function($join, $categorySlug) {
+                        return $join->where('categories.slug', '=', $categorySlug);
+                    });
+            })
+            ->select('tasks.*', 'states.name as state_name', 'priorities.name as priority_name')
+            ->get();
+        */
+
+        return $this->with([
+                'currentPriority',
+                'currentState',
+                'category',
+            ])
+            ->join('priorities', 'tasks.current_priority_id', '=', 'priorities.id')
+            ->join('states', function($join) use ($stateSlug) {
+                $join->on('tasks.current_state_id', '=', 'states.id')
+                    ->where('states.slug', '=', $stateSlug);
+            })
+            ->join('categories', function($join) use ($categorySlug) {
+                $join->on('tasks.category_id', '=', 'categories.id')
+                    ->where('categories.user_id', '=', Auth::id())
+                    ->when($categorySlug, function($join, $categorySlug) {
+                        return $join->where('categories.slug', '=', $categorySlug);
+                    });
+            })
+            ->get();
+
     }
 }
